053694cf160f001aac63e7f6f53425625c5b4dc4
Arantza <aer.castro.dessavre@gmail.com>
2023-03-06 11:27:59 -0600

    Initial Commit

---
+++ PrimerApp/PrimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerAppApp.swift
+//  PrimerApp
+//
+//  Created by CEDAM02 on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PrimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
